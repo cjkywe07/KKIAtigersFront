0e66b07c97862eeae8cb5a6396a1eaa6a3ed80b3
JiHye0214 <qkrwlgp1526@gmail.com>
2024-01-15 13:39:28 +0900

    header, mypage

--- js/header.js
+++ js/header.js
@@ -1,9 +1,10 @@
 const $menuArr = document.querySelectorAll("header .header-menu");
-const setMenuArr = ["games.html", "market.html", "community.html"]; // intellij --> .html 지우기
+const setMenuArr = ["game.html", "market.html", "community.html"]; // intellij --> .html 지우기
 
 // 어느 페이지
 let link = document.location.href;
 const linkArr = link.split("/");
+console.log(linkArr[3])
 
 setMenuArr.forEach((menu, index) => {
     if(menu == linkArr[3]) {
